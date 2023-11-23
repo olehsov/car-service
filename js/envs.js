bdbe96431d8f62b6b603b0cef67fe12776410884
Oleh Skrypnyk <oskrypnyk@griddynamics.com>
2023-11-23 17:16:28 +0100

    Update responsives

---
+++ js/envs.js
@@ -0,0 +1,6 @@
+const BOT_TOKEN = '6199783028:AAE9eiRsxImc7fl6tGhirx-yDH1ubpFtozM';
+const TELEGRAM_URL = `https://api.telegram.org/bot${BOT_TOKEN}`
+const TELEGRAM_CHAT_ID = 549446871;
+const PHONE = '+380979034777';
+const GEO_LINK = 'https://maps.app.goo.gl/T4MzyWD7CYGcHX1M7?g_st=ic';
+const MOVE_SIZE = 161;
--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
 "use strict"
 
-const BOT_TOKEN = '6199783028:AAE9eiRsxImc7fl6tGhirx-yDH1ubpFtozM';
-const TELEGRAM_URL = `https://api.telegram.org/bot${BOT_TOKEN}`
-const TELEGRAM_CHAT_ID = 549446871;
-const PHONE = '+380979034777';
-const GEO_LINK = 'https://maps.app.goo.gl/T4MzyWD7CYGcHX1M7?g_st=ic';
-const MOVE_SIZE = 161;
-
 const sideBarForm = document.getElementById('side-bar-form');
 const modal = document.getElementById('modal-call4');
 const checkPriceButton = document.getElementById('check-price');
